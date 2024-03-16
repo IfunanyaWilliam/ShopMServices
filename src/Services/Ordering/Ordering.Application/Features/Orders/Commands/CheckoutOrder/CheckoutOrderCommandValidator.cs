14b8b0f7669cf7e5db33306c7a05604219965a87
Ifunanya <williamifunanya@gmail.com>
2024-03-16 18:32:13 +0100

    Add UpdateOrderCommnad classes

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -11,12 +11,20 @@ namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
                 .NotNull()
                 .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters.");
 
-            RuleFor(p => p.EmailAddress)
-               .NotEmpty().WithMessage("{EmailAddress} is required.");
-
             RuleFor(p => p.TotalPrice)
                 .NotEmpty().WithMessage("{TotalPrice} is required.")
                 .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+
+            RuleFor(p => p.EmailAddress)
+               .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(p => p.FirstName)
+                .NotEmpty().WithMessage("{FirstName} is required")
+                .MaximumLength(50).WithMessage("{FirstName} must not exceed 50 characters.");
+
+            RuleFor(p => p.LastName)
+                .NotEmpty().WithMessage("{FirstName} is required")
+                .MaximumLength(50).WithMessage("{LastName} must not exceed 50 characters.");
         } 
     }
 }
